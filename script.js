cb141eaec928ba621e78ff709808443af9ede29e
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2025-12-22 22:42:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ $(document).ready(function () {
   $("#toggle1").click(function () {
       $("#article1").slideToggle();
     });
-  });
 $("#toggle2").click(function () {
       $("#article2").slideToggle();
     });
-  });
+ });
